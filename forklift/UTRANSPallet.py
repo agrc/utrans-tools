32df523ca76be3659a9ae04fec732cbe9ed38ff6
Scott Davis <stdavis@utah.gov>
2026-08-25 10:03:13 -0600

    feat(forklift): create forklift pallet for data needed by attribute rules
    Closes #22

---
+++ forklift/UTRANSPallet.py
@@ -0,0 +1,26 @@
+from forklift import Pallet
+from pathlib import Path
+
+
+class UTRANSPallet(Pallet):
+    def __init__(self):
+        super().__init__()
+
+    def build(self, configuration):
+        utrans = Path(self.garage) / "UTRANS.sde"
+        sgid = Path(self.garage) / "SGID.sde"
+
+        for source in [sgid, utrans]:
+            if not source.exists():
+                raise RuntimeError(f"{source.name} not found in {self.garage}")
+
+        self.add_crates(
+            [
+                "Counties",
+                "Municipalities",
+                "ZipCodes",
+                "NationalGrid",
+                "AddressSystemQuadrants",
+            ],
+            {"source_workspace": sgid, "destination_workspace": utrans},
+        )
